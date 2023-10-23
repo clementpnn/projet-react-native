3a7c2bfbdf36565350a3f70e27edfb83d25bb159
Clément <95907390+clementpnn@users.noreply.github.com>
2023-10-24 00:02:16 +0200

    basic game front

--- server/main.py
+++ server/main.py
@@ -1,1 +1,17 @@
-print("hello world")
+from flask import Flask, send_from_directory
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return send_from_directory('public', 'index.html')
+
+@socketio.on('move')
+def handle_move(json):
+    print('Received move: ' + str(json))
+    socketio.emit('move', json, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
