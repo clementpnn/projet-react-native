9e859ac3018a2a4067089bf203ef66d6a02db1cf
Clément <95907390+clementpnn@users.noreply.github.com>
2023-11-08 21:59:29 +0100

    add game

--- app/src/App.js
+++ app/src/App.js
@@ -3,7 +3,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './pages/Home';
 import { WebSocketProvider } from './context/WebSocketContext';
-import CreateGameScreen from './pages/CreateGame';
 
 const Stack = createStackNavigator();
 
@@ -13,7 +12,6 @@ export default function App() {
       <WebSocketProvider>
         <Stack.Navigator initialRouteName="Home">
           <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
-          <Stack.Screen name="GameScreen" component={CreateGameScreen} options={{ headerShown: false }} />
         </Stack.Navigator>
       </WebSocketProvider>
     </NavigationContainer>
--- server/main.py
+++ server/main.py
@@ -1,36 +1,20 @@
+# backend.py
 import asyncio
 import websockets
-import json
-from random import randint
 
-rooms = {}
+connected = set()
 
-async def handler(websocket, path):
-    global rooms
-    
-    async for message in websocket:
-        data = json.loads(message)
-        action = data.get("action")
-        
-        if action == "create":
-            room_code = randint(1000, 9999)
-            rooms[room_code] = {'host': websocket, 'guest': None}
-            await websocket.send(json.dumps({"action": "created", "room_code": str(room_code)}))
-            
-        elif action == "join":
-            room_code = int(data.get("room_code"))
-            room = rooms.get(room_code)
-            
-            if room and not room['guest']:
-                room['guest'] = websocket
-                await room['host'].send(json.dumps({"action": "start_game"}))
-                await room['guest'].send(json.dumps({"action": "start_game"}))
-                
-            elif not room:
-                await websocket.send(json.dumps({"action": "error", "message": "Room does not exist"}))
-            else:
-                await websocket.send(json.dumps({"action": "error", "message": "Room is full"}))
+async def server(websocket, path):
+    connected.add(websocket)
+    try:
+        async for message in websocket:
+            for conn in connected:
+                if conn != websocket:
+                    await conn.send(message)
+    finally:
+        connected.remove(websocket)
 
-start_server = websockets.serve(handler, "0.0.0.0", 8080)
+start_server = websockets.serve(server, "localhost", 6789)
+
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
